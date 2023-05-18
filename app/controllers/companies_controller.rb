6c30b47c49ae931299c99da991114b1bb0880d17
brendan power <brendan.power@kellogg.northwestern.edu>
2023-05-18 03:46:34 +0000

    all code along

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,24 +2,35 @@ class CompaniesController < ApplicationController
 
   def index
     # find all Company rows
+    @companies = Company.all 
     # render companies/index view
   end
 
-  # def show
-  #   # find a Company
-  #   # render companies/show view with details about Company
-  # end
+  def show
+    # find a Company
+    @company = Company.find_by({"id" => params["id"]})
+    @contacts = Contact.where({"company_id" => @company["id"]})
 
-  # def new
-  #   # render view with new Company form
-  # end
+    # render companies/show view with details about Company
+  end
+
+  def new
+    @company = Company.new
+    # render view with new Company form
+  end
   
-  # def create
-  #   # start with a new Company
-  #   # assign user-entered form data to Company's columns
-  #   # save Company row
-  #   # redirect user
-  # end
+  def create
+    @company=Company.new
+    # start with a new Company
+    # assign user-entered form data to Company's columns
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["state"] = params["company"]["state"]
+    # save Company row
+    @company.save
+    # redirect user
+    redirect_to "/companies" 
+  end
 
   # def edit
   #   # find a Company
